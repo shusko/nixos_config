4513c8e479c31292fae572547f758936937004b7
Sean Huskey <seanhuskey@gmail.com>
2024-04-01 08:58:45 -0400

    Restore maximized/minimized keys

--- users/sean/awesome_wm/rc.lua
+++ users/sean/awesome_wm/rc.lua
@@ -462,19 +462,19 @@ clientkeys = mytable.join(
         { description = "move to screen", group = "client" }),
     awful.key({ modkey, }, "t", function(c) c.ontop = not c.ontop end,
         { description = "toggle keep on top", group = "client" }),
-    -- awful.key({ modkey, }, "n",
-    --     function(c)
-    --         -- The client currently has the input focus, so it cannot be
-    --         -- minimized, since minimized clients can't have the focus.
-    --         c.minimized = true
-    --     end,
-    --     { description = "minimize", group = "client" }),
-    -- awful.key({ modkey, }, "m",
-    --     function(c)
-    --         c.maximized = not c.maximized
-    --         c:raise()
-    --     end,
-    --     { description = "(un)maximize", group = "client" }),
+    awful.key({ modkey, }, "n",
+        function(c)
+            -- The client currently has the input focus, so it cannot be
+            -- minimized, since minimized clients can't have the focus.
+            c.minimized = true
+        end,
+        { description = "minimize", group = "client" }),
+    awful.key({ modkey, }, "m",
+        function(c)
+            c.maximized = not c.maximized
+            c:raise()
+        end,
+        { description = "(un)maximize", group = "client" }),
     awful.key({ modkey, "Control" }, "m",
         function(c)
             c.maximized_vertical = not c.maximized_vertical
